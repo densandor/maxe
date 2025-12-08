36b5d4dcfa8d4fe1af7f043eca9b3f9b8157572b
sandor <sandor.den6@gmail.com>
2025-12-08 13:11:34 +0000

    Removed unnecessary log

--- TheSimulator/TheSimulator/Trade.cpp
+++ TheSimulator/TheSimulator/Trade.cpp
@@ -6,13 +6,6 @@ Trade::Trade(TradeID id, Timestamp timestamp, OrderDirection direction, OrderID 
 #include <iostream>
 
 void Trade::printHuman() const {
-	/*std::cout << std::to_string(m_id) << "\t"
-		<< std::to_string(m_timestamp) << "\t"
-		<< std::to_string(m_aggressingOrderID) << "\t"
-		<< (m_direction == OrderDirection::Sell ? "SELL" : "BUY ") << "\t"
-		<< std::to_string(m_restingOrderID) << "\t"
-		<< std::to_string(m_volume) << "\t"
-		<< m_price.toCentString();*/
 	std::cout << "Trade " + std::to_string(m_id)
 		<< " occurred at time " << std::to_string(m_timestamp)
 		<< ", matching order " << std::to_string(m_aggressingOrderID) << " vs. " << std::to_string(m_restingOrderID) 
